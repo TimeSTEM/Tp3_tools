2b1dffc5a0f27cea3c09fb41bd05f0c44d1bef61
yvesauad <yvesauad@gmail.com>
2021-02-04 16:59:30 +0100

    YA: 2D image

--- rusty_tcp/src/main.rs
+++ rusty_tcp/src/main.rs
@@ -19,23 +19,24 @@ fn correct_x_position(chip_index: u8, pos: u8) -> usize {
 fn build_data(data: &Vec<u8>) -> Vec<u8> {
     
     let file_data = data;
-    let mut final_data = vec![0; 2048];
+    //let mut final_data = vec![0; 2048];
+    let mut final_data = vec![0; 524288];
     let mut index = 0;
     
     let mut total_size: u16;
     let mut chip_index: u8;
+    let mut id: u8;
     /*
     let mut _spidr: u16;
     let mut _ftoa: u8;
     let mut _tot: u16;
     let mut _toa: u16;
     let mut _y: u16;
-    let mut _spix: u16;
     */
-    let mut _pix: u8;
-    let mut _dcol: u8;
+    let mut pix: u8;
+    let mut dcol: u8;
+    let mut spix: u8;
 
-    let mut x: u8;
     
     while index < file_data.len() {
         assert_eq!(Some(&[84, 80, 88, 51][..]), file_data.get(index..index+4));
@@ -43,29 +44,32 @@ fn build_data(data: &Vec<u8>) -> Vec<u8> {
         chip_index = file_data[index+4];
         total_size = (file_data[index+6] as u16) | (file_data[index+7] as u16)<<8;
 
-        let _nloop = total_size / 8;
-        for _i in 0.._nloop {
+        for _i in 0..total_size/8 {
             /*
             _spidr = (file_data[index+8] as u16) | (file_data[index+9] as u16)<<8;
             _ftoa = file_data[index+10] & 15;
             _tot = ((file_data[index+10] & 240) as u16)>>4 | ((file_data[index+11] & 63) as u16)<<4;
             _toa = ((file_data[index+11] & 192) as u16)>>6 | (file_data[index+12] as u16)<<2 | ((file_data[13] & 15) as u16)<<10;
             */
-            _pix = (file_data[index+13] & 112)>>4;
-            //_spix = ((file_data[index+13] & 128) as u16)>>5 | ((file_data[index+14] & 31) as u16)<<3;
-            _dcol = ((file_data[index+14] & 224))>>4 | ((file_data[index+15] & 15))<<4;
+            pix = (file_data[index+13] & 112)>>4;
+            spix = ((file_data[index+13] & 128))>>5 | ((file_data[index+14] & 31))<<3;
+            dcol = ((file_data[index+14] & 224))>>4 | ((file_data[index+15] & 15))<<4;
+            id = (file_data[index+15] & 240) >> 4;
 
-            x = _dcol | _pix >> 2;
-            //_y = _spix | (_pix & 3);
+            let x: u8 = dcol | pix >> 2;
+            let y: usize = (spix | (pix & 3)) as usize;
 
             //let x = x as usize;
             let x = correct_x_position(chip_index, x);
 
-            final_data[2*x]+=1;
-            if final_data[2*x]==255 {
-                final_data[(2*x+1)] += 1;
-                final_data[2*x] = 0;
+            if id==11 {
+                final_data[2*x +2048*y]+=1;
+                if final_data[2*x+2048*y]==255 {
+                    final_data[(2*x+2048*y+1)] += 1;
+                    final_data[2*x+2048*y] = 0;
+                }
             }
+
             index = index + 8;
         }
         index = index + 8;
@@ -150,18 +154,18 @@ fn main() {
         */
         loop {
             let start = Instant::now();
-            let msg = create_header(0.352, counter, 2048, 16, 1024, 1);
+            let msg = create_header(0.352, counter, 524288, 16, 1024, 256);
 
-            if has_data(counter)==false {
+            while has_data(counter)==false {
                 counter = 0;
             }
             
             let (mydata, _) = open_and_read(counter);
             let my_real_data = build_data(&mydata);
 
+            let elapsed = start.elapsed();
             sock.write(&msg).expect("Header not sent.");
             sock.write(&my_real_data).expect("Data not sent.");
-            let elapsed = start.elapsed();
             println!("{} and {:?}", counter, elapsed);
             counter+=1;
         }
