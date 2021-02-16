d28ae3bed05e765b4524ba11fccc8d83447b1acf
yvesauad <yvesauad@gmail.com>
2021-02-16 16:28:14 +0100

    YA: minor add

--- TCPFiletoStream/receiveServer.py
+++ TCPFiletoStream/receiveServer.py
@@ -3,10 +3,10 @@ import socket
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8098
 
+print('waiting for TPX3')
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#serv.settimeout(5)
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
 serv.bind((SERVER_HOST, SERVER_PORT))
 serv.listen()
 conn, addr = serv.accept()
