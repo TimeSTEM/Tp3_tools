f619745aade32ed7e746943e8c59d5b905d50c27
yvesauad <yvesauad@gmail.com>
2021-02-24 13:12:36 +0100

    YA: library good

--- rusty_tcp/src/lib.rs
+++ rusty_tcp/src/lib.rs
@@ -172,6 +172,57 @@ impl TdcType {
             TdcType::TdcTwoFallingEdge => "Two_Falling",
         }
     }
+    
+    pub fn count_tdcs(tdc_vec: &Vec<(f64, TdcType)>) -> [usize; 4] {
+        let mut result = [0usize; 4];
+        for (time, tdc_type) in tdc_vec {
+            match tdc_type {
+                TdcType::TdcOneRisingEdge => result[0]+=1,
+                TdcType::TdcOneFallingEdge => result[1]+=1,
+                TdcType::TdcTwoRisingEdge => result[2]+=1,
+                TdcType::TdcTwoFallingEdge => result[3]+=1,
+            }
+        }
+        result
+    }
+
+    pub fn check_all_tdcs(min: &[usize; 4], tdc_vec: &Vec<(f64, TdcType)>) -> bool {
+        let val = TdcType::count_tdcs(tdc_vec);
+        let mut how_many = val.iter().zip(min.iter()).filter(|(min, val)| min>=val).count();
+        match how_many {
+            4 => true,
+            _ => false,
+        }
+    }
+
+    pub fn check_each_tdcs(min: &[usize; 4], tdc_vec: &Vec<(f64, TdcType)>) -> Vec<bool> {
+        let val = TdcType::count_tdcs(tdc_vec);
+        let seq = val.iter().zip(min.iter()).map(|(min, val)| min>=val).collect::<Vec<bool>>();
+        seq
+    }
+    
+    pub fn vec_from_tdc(tdc_vec: &Vec<(f64, TdcType)>, tdc_type: u8) -> Vec<&f64> {
+        let result: Vec<_> = tdc_vec.iter()
+            .filter(|(_time, tdct)| tdct.associate_value()==tdc_type)
+            .map(|(time, _tdct)| time)
+            .collect();
+        result
+    }
+
+    pub fn last_time_from_tdc(tdc_vec: &Vec<(f64, TdcType)>, tdc_type: u8) -> f64 {
+        let last_time = tdc_vec.iter()
+            .filter(|(_time, tdct)| tdct.associate_value()==tdc_type)
+            .map(|(time, _tdct)| time)
+            .last().unwrap();
+        *last_time
+    }
+                
+    pub fn howmany_from_tdc(tdc_vec: &Vec<(f64, TdcType)>, tdc_type: u8) -> usize {
+        let counter = tdc_vec.iter()
+            .filter(|(_time, tdct)| tdct.associate_value()==tdc_type)
+            .count();
+        counter
+    }
 
 }
 
