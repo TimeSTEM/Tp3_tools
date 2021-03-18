c3fe51d638b8eed515e24e4caf8a39485f5d313e
yvesauad <yvesauad@gmail.com>
2021-03-18 19:32:48 +0100

    add HT raw analysis

---
+++ RawAnalysis/ht_raw.py
@@ -0,0 +1,164 @@
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import time
+import os
+
+xL = list() #x position
+yL = list() #Y position
+Tafter = list() #Global Time
+Tdif = list() #Tdc time diferences
+last_tdc = [0.0, 0.0, 0.0, 0.0, 0.0]
+
+i = [0, 0] #Counter. First index is electron event and second is tdc event.
+final_tdc = 0 #Last tdc time received
+start = time.time() 
+
+#FOLDER = '../TCPFiletoStream/GainRawTP3/25-53-25262(132)'
+FOLDER = '../TCPFiletoStream/18-03-2021'
+
+tL_int = 1
+size = int(60 / tL_int)
+tL = [list() for i in range(size)]
+
+WIDTH = 60000e-9
+DELAY = 0000e-9
+MIN_HOR = 100
+MAX_HOR = 190
+
+def check_if_in(ele_time, tdc_time_list):
+    for val in tdc_time_list[::-1]:
+        if ele_time > val + DELAY and ele_time < val + DELAY + WIDTH:
+            return (True, val)
+    return (False, 0)
+
+
+for data in os.listdir(FOLDER):# in datas:
+    print(f'Looping over file {data}.')
+    #if i[0] != 0: break
+    with open(os.path.join(FOLDER, data), "rb") as f:
+        all_data = f.read()
+        index = 0 #Reading index.
+        while True:
+            byte = all_data[index: index+8] #8 bytes header
+            byte=byte[::-1]
+            if byte==b'': #Breaks if there is none
+                break
+            tpx3_header = byte[4:8] #4 bytes=32 bits
+            assert tpx3_header==b'3XPT' #must be this
+            chip_index = byte[3] #1 byte
+            #mode = byte[2] #1 byte
+            size_chunk1 = byte[1] #1 byte
+            size_chunk2 = byte[0] #1 byte
+            total_size = size_chunk1+ size_chunk2*256 #Number of pixels in chunk
+            for j in range(int(total_size/8)):
+                index+=8 #Go to next 8 bytes
+                byte = all_data[index:index+8]
+                byte=byte[::-1]
+                id = (byte[0] & 240)>>4 #240 = 1111 0000. 240 >> 4 = 0000 11111.
+                if id==11: #11 = 0xb (Electron Event)
+                    i[0]+=1 #Increment Electron Event
+                    
+                    toa = ((byte[2] & 15)<<10) | ((byte[3] & 255)<<2) | ((byte[4] & 192)>>6)
+                    tot = ((byte[4] & 63)<<4) | ((byte[5] & 240)>>4)
+                    ftoa = (byte[5] & 15)
+                    spidr = ((byte[6] & 255)<<8) + ((byte[7] & 255))
+                    ctoa = toa<<4 | ~ftoa & 15
+                    
+                    spidrT = spidr * 25.0 * 16384.0
+                    #toa_ns = toa * 25.0
+                    tot_time= tot * 25.0 / 1e9
+                    global_time = (spidrT + ctoa * 25.0/16.0)/1e9
+
+                    
+
+                    #if global_time > last_tdc + DELAY and global_time < last_tdc + DELAY + WIDTH:
+                    res = check_if_in(global_time, last_tdc)
+                    if res[0]:
+                        tdc_ref = res[1]
+
+                        dcol = ((byte[0] & 15)<<4) | ((byte[1] & 224)>>4)
+                        spix = ((byte[1] & 31)<<3) | ((byte[2] & 128)>>5)
+                        pix = (byte[2] & 112)>>4
+
+                        x = dcol | pix>>4
+                        y = int(spix | (pix & 3))
+
+    
+                        if chip_index==0:
+                            x = 255 - x
+                            y = y
+                        elif chip_index==1:
+                            x = 255*4 - x
+                            y = y
+                        elif chip_index==2:
+                            x = 255*3 - x
+                            y = y
+                        elif chip_index==3:
+                            x = 255*2 - x
+                            y = y
+                        
+                        if x>MIN_HOR and x<MAX_HOR:
+                            xL.append(x)
+                            yL.append(y)
+                            Tafter.append((global_time - tdc_ref)*1e6)
+                            j = int( (global_time - tdc_ref)*1e6/tL_int)
+                            tL[j].append(x)
+                        #if (global_time - tdc_ref)*1e6 > 70:
+                        #    print(global_time, last_tdc[-1], (global_time - last_tdc[-1])*1e6)
+
+                
+                elif id==6: #6 = 0xb. This is a tdc event.
+                    i[1]+=1 #Increment tdc event.
+                    triggerType = byte[0] & 15 #15 = 1111. Get trigger Type.
+                    if triggerType==14:
+                        coarseT = ((byte[2] & 15)<<31) + ((byte[3] & 255)<<23) + ((byte[4] & 255)<<15) + ((byte[5] & 255)<<7) + ((byte[6] & 254)>>1)
+                        fineT = ((byte[6] & 1)<<3) + ((byte[7] & 224)>>5)
+                        tdcT = coarseT * (1/320e6) + fineT*260e-12
+                        Tdif.append((tdcT - last_tdc[-1])*1e6)
+                        last_tdc.pop(0)
+                        last_tdc.append(tdcT)
+                        #print(last_tdc[-1])
+                else: 
+                    pass
+            index+=8 #Goes back to next header
+                
+finish = time.time()
+print(f'Total time is {finish-start} with {i} events. Last tdc is at {last_tdc}. Number of positional electrons are {len(xL)}. Number of temporal electrons are {len(Tafter)}.')
+
+x = list()
+y = list()
+
+for i, val in enumerate(tL):
+    x.append(i*tL_int)
+    y.append(numpy.mean(val))
+
+fig, ax = plt.subplots(1, 1, dpi=160)
+ax.plot(x, y, linewidth=2.0, color='red', label='X average')
+ax.scatter(x, y)
+
+ax.set_ylabel('Avg[X] pixel')
+ax.set_xlabel('Time delay (us)')
+ax.legend()
+
+plt.show()
+
+
+fig, ax = plt.subplots(3, 1, dpi=160)
+ax[0].hist2d(xL, yL, bins=100, range=([0, 1024], [0, 256]), norm=mcolors.PowerNorm(0.3))
+ax[0].axvline(x=MIN_HOR, color='red')
+ax[0].axvline(x=MAX_HOR, color='white')
+ax[1].hist(Tafter, bins=200, density = True, range=(DELAY*1e6, (DELAY+WIDTH)*1e6))
+ax[2].hist(Tdif, bins=200, density = True)
+                
+ax[0].set_ylabel('Y')
+ax[0].set_xlabel('X')
+
+ax[1].set_ylabel('Event counts')
+ax[1].set_xlabel('Time Elapsed from TDC (us)')
+
+ax[2].set_ylabel('Event counts')
+ax[2].set_xlabel('TDC time distribution (us)')
+
+plt.show()
+
