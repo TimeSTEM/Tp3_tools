7e9c7c53c654d077d55139dad55f42b5bf2b3b5c
yvesauad <yvesauad@gmail.com>
2021-04-30 11:43:53 +0200

    YA: put EELS spectra together

--- rusty_tcp/src/bin/histo.py
+++ rusty_tcp/src/bin/histo.py
@@ -2,16 +2,15 @@ import numpy
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-xmin = 0
-xmax = 1024
-
 t = numpy.loadtxt("tH.txt", delimiter=',')
 x = numpy.loadtxt("xH.txt", delimiter=',')
-indexes = (x>=xmin) & (x<=xmax)
+indexes = (x>=0) & (x<=1024)
 
-fig, ax = plt.subplots(1, 1)
+fig, ax = plt.subplots(1, 2)
 plt.subplots_adjust(left=0.25, bottom=0.25)
-ax.hist(t[indexes], bins=25)
+ax[0].hist(t[indexes], bins=25)
+ax[1].hist(x[indexes], bins=1024, range=(0, 1024))
+l = ax[1].axvspan(10, 12, color='red', alpha=0.25)
 
 axcolor = 'lightgoldenrodyellow'
 axc = plt.axes([0.25, 0.1, 0.6, 0.03], facecolor = axcolor)
@@ -23,14 +22,14 @@ def update(val):
     xmax = sc.val + sint.val
     xmin = sc.val - sint.val
     indexes = (x>=xmin) & (x<=xmax)
-    print(xmin, xmax, len(t[indexes]))
-    ax.cla()
-    ax.hist(t[indexes], bins=25)
+    ax[0].cla();
+    ax[0].hist(t[indexes], bins=25)
+    tempPol = (l.get_xy())
+    tempPol[:, 0] = [xmin, xmin, xmax, xmax, xmin]
+    l.set_xy(tempPol)
+
 
 sc.on_changed(update)
 sint.on_changed(update)
 
-
-ax.set_ylabel('E-Counts')
-ax.set_xlabel('Time (s)')
 plt.show()
