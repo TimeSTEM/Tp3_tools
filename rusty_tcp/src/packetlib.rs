4def4adc836c89762e9992a0c1ae18ee41eb9721
yvesauad <yvesauad@gmail.com>
2021-11-18 08:26:05 +0100

    YA: tiny improvement in packetlib

--- rusty_tcp/src/packetlib.rs
+++ rusty_tcp/src/packetlib.rs
@@ -5,7 +5,10 @@ pub trait Packet {
     fn ci(&self) -> usize;
     fn data(&self) -> &[u8];
     fn x(&self) -> usize {
-        let temp = (((self.data()[6] & 224)>>4 | (self.data()[7] & 15)<<4) | (((self.data()[5] & 112)>>4)>>2)) as usize;
+        
+        //let temp = (((self.data()[6] & 224)>>4 | (self.data()[7] & 15)<<4) | (((self.data()[5] & 112)>>4)>>2)) as usize;
+        let temp = ((self.data()[6] & 224)>>4 | (self.data()[7] << 4) | ((self.data()[5] & 112) >> 6)) as usize;
+
         match self.ci() {
             0 => 255 - temp,
             1 => 256 * 4 - 1 - temp,
@@ -53,13 +56,16 @@ pub trait Packet {
     
     fn electron_time(&self) -> usize {
         let spidr = (self.data()[0] as usize) | (self.data()[1] as usize)<<8;
-        let toa = ((self.data()[3] & 192) as usize)>>6 | (self.data()[4] as usize)<<2 | ((self.data()[5] & 15) as usize)<<10;
+        
+        //let toa = ((self.data()[3] & 192) as usize)>>6 | (self.data()[4] as usize)<<2 | ((self.data()[5] & 15) as usize)<<10;
         //let toa = ((self.data()[3] >> 6) as usize) | (self.data()[4] as usize)<<2 | ((self.data()[5] << 4) as usize)<<6;
+        let toa = ((self.data()[3] >> 6) as usize) | (self.data()[4] as usize)<<2 | ((self.data()[5] & 15) as usize)<<10;
+        
         let ftoa = (self.data()[2] & 15) as usize;
         //let ftoa2 = (!self.data()[2] & 15) as usize;
         let ctoa = (toa << 4) | (!ftoa & 15);
         //let ctoa2 = (toa << 4) | ftoa2;
-        //assert_eq!(ctoa, ctoa2);
+        
         spidr * 25 * 16384 + ctoa * 25 / 16
     }
 
