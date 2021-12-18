cdf72bb06c8ab50ac0fddc03a1ec239e728dac94
yvesauad <yvesauad@gmail.com>
2021-12-18 10:08:05 +0100

    YA: updating histo to coincidence measurements

--- rusty_tcp/src/bin/histo.py
+++ rusty_tcp/src/bin/histo.py
@@ -12,79 +12,38 @@ x = numpy.loadtxt("cspec.txt", delimiter=',')
 x = numpy.reshape(x, (256, 1024))
 x = numpy.sum(x, axis=0)
 xH = numpy.loadtxt("xH.txt", delimiter=',')
-yH = numpy.loadtxt("yH.txt", delimiter=',')
-indexes = numpy.where(yH>0)[0]
-xH = xH[indexes]
-t = t[indexes]
+#yH = numpy.loadtxt("yH.txt", delimiter=',')
+#indexes = numpy.where(xH<1025)[0]
+#xH = xH[indexes]
+#t = t[indexes]
 
-#cs = numpy.loadtxt("cs.txt", delimiter=',')
-#stot = numpy.loadtxt("stot.txt", delimiter=',')
-#print('CS mean is: ', numpy.mean(cs))
-
-#xT = numpy.divide(xT, numpy.max(xT))
-#x = numpy.divide(x, numpy.max(x))
 cRatio = numpy.divide(x, xT)
 
-fig, ax = plt.subplots(1, 3, dpi=180, sharex = False)
-#plt.subplots_adjust(left=0.25, bottom=0.25)
+#Bins
+tmax = int(numpy.max(t))
+tmin = int(numpy.min(t))
+tbin = int((tmax - tmin)/(4*1.5625))
+
+fig, ax = plt.subplots(1, 2, dpi=180, sharex = False)
 ax2 = ax[0].twinx()
 ax[0].plot(disparray, x, label='Correlated Data')
 ax[0].plot(disparray, xT, alpha=0.8, ls='--', color='red', lw=2, label='Total Data')
 ax2.scatter(disparray, cRatio, c='green', s=5, marker='x', label='Ratio')
-ax[1].hist(t, bins=200, range = (-100, 100))
-ax[2].hist(xH, bins=1024, range = (0, 1024))
+ax[1].hist(t, bins=tbin, range=(tmin, tmax))
+#ax[2].hist(xH, bins=1024, range = (0, 1024))
 
 ax[0].set_xlabel('Energy (eV)')
 ax[0].set_ylabel('$\Gamma^{Loss}$ (A.U.)')
 ax2.set_ylabel('Coincidence Ratio', c='green')
-
-#ax[1].hist(stot, bins=100, range=[0, 600])
-#ax[0, 0].hist2d(stot, cs, range=[[0, 600], [1, 18]], bins=[10, 17], norm=mcolors.PowerNorm(0.5), cmap='inferno')
-#ax[1, 1].hist(t, bins=50)
-
-#ax[0].set_xlabel('ToT Sum (A.U.)')
-#ax[0].set_xlabel('Cluster Radius std (pixels)')
-#ax[0].set_ylabel('Cluster Size')
-
-#ax[1].set_xlabel('ToT Sum (A.U.)')
-##ax[1].set_xlabel('Cluster Radius std (pixels)')
-#ax[1].set_ylabel('Frequency')
-
-#ax[0].hist(t[indexes], bins=25)
-#ax[1].hist(x[indexes], bins=1024, range=(0, 1024))
-#ax[1].hist2d(x[indexes], y[indexes], bins=[101, 51], range=[[0, 1024], [50, 225]])
-#l = ax[1].axvspan(10, 12, color='red', alpha=0.25)
-
-""""
-axcolor = 'lightgoldenrodyellow'
-axc = plt.axes([0.25, 0.1, 0.6, 0.03], facecolor = axcolor)
-axint = plt.axes([0.25, 0.15, 0.6, 0.03], facecolor = axcolor)
-sc = Slider(axc, 'Center', 0, 1024, valinit=0, valstep=1)
-sint = Slider(axint, 'Int.', 0, 1024, valinit=0, valstep=1)
-
-def update(val):
-    xmax = sc.val + sint.val
-    xmin = sc.val - sint.val
-    indexes = (x>=xmin) & (x<=xmax)
-    ax[0].cla();
-    ax[0].hist(t[indexes], bins=25)
-    tempPol = (l.get_xy())
-    tempPol[:, 0] = [xmin, xmin, xmax, xmax, xmin]
-    l.set_xy(tempPol)
-
-
-sc.on_changed(update)
-sint.on_changed(update)
-"""
-
 ax[0].legend(loc='upper center')
-#ax2.legend(loc='upper center')
-#fig.legend()
+
 plt.tight_layout()
 plt.savefig('coinc.svg')
 plt.show()
 
 fig, ax = plt.subplots(1, 1, dpi=180, sharex = False)
-ax.hist2d(xH, t, bins=[1024, 100], range = [[0, 1024], [-50, 50]])
+ax.hist2d(xH, t, bins=[1024, tbin], range = [[0, 1024], [tmin, tmax]], cmap = 'inferno', norm = mcolors.PowerNorm(0.1))
+ax.set_xlabel('Energy (pixels)')
+ax.set_ylabel('Time delay (ns)')
 plt.tight_layout()
 plt.show()
