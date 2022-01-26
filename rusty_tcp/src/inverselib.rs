881cb35dd84bdfd401a142b06db1f5471ad80448
yvesauad <yvesauad@gmail.com>
2022-01-26 17:47:52 +0100

    YA: working on the inverselib

--- rusty_tcp/src/inverselib.rs
+++ rusty_tcp/src/inverselib.rs
@@ -5,7 +5,16 @@ pub mod inverselib {
         time: usize,
     }
 
-    fn time() {
-        println!("k");
+    impl InvPacketEELS {
+        fn time_to_ticks(&self) -> (usize, usize, usize) {
+            let spidr_ticks = self.time / 409_600;
+            let ctoa = self.time % 409_600;
+            let toa_ticks = ctoa / 25;
+            let ftoa_ticks = ctoa % 25;
+            (spidr_ticks, toa_ticks, ftoa_ticks)
+        }
     }
+
+
+
 }
