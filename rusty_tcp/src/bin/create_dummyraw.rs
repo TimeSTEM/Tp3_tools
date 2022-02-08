4a12f130763342d2f240b3d14a1a672a4ab3aef3
yvesauad <yvesauad@gmail.com>
2022-02-08 15:36:31 +0100

    YA: packet created. Must export

--- rusty_tcp/src/bin/create_dummyraw.rs
+++ rusty_tcp/src/bin/create_dummyraw.rs
@@ -1,37 +1,49 @@
+use std::fs::File;
+use std::io::prelude::*;
 use timepix3::packetlib::*;
 use rand_distr::{Poisson, Exp, Normal, Distribution};
 use rand::distributions::Uniform;
 use rand::{thread_rng};
 
 fn main() {
-    //let inv = InversePacket::new_inverse_electron(10, 10, 10_000_000_000);
-    //let tdc_inv = InversePacket::new_inverse_tdc(10_000_000_000);
 
-
-    let npoints = 1_000;
     let normal_01 = Normal::new(128.0, 3.0).unwrap();
     let normal_02 = Normal::new(638.0, 10.0).unwrap();
     let uni = Uniform::new(0.0, 1.0);
-    let poisson = Poisson::new(1).unwrap(); //~30 pA or 0.2 e-/ns;
+    let poisson = Poisson::new(1.0).unwrap(); //~30 pA or 0.2 e-/ns;
     let exp = Exp::new(0.2).unwrap();
 
+    let mut electron: InversePacket;
+    let mut photon:Option<InversePacket> = None;
+    let mut x: usize;
+    let mut data: Vec<[u8; 16]> = Vec::new();
 
     let mut global_time = 0;
     for _ in 0..1_000 {
         let dt = exp.sample(&mut rand::thread_rng()) as usize;
         global_time += dt;
         if uni.sample(&mut thread_rng()) < 0.25 {
-            //create_electron_gaussian_01
+            x = normal_01.sample(&mut rand::thread_rng()) as usize;
         } else {
-            //create_electron_gaussian_02
+            x = normal_02.sample(&mut rand::thread_rng()) as usize;
             if uni.sample(&mut thread_rng()) > 0.5 {
-                //create_photon;
+                photon = Some(InversePacket::new_inverse_tdc(dt));
+            }
+            else {
+                photon = None;
             }
         }
-        println!("{} and {}", dt, global_time);
+        electron = InversePacket::new_inverse_electron(x, 128, global_time);
+        data.push(electron.create_electron_array());
+        if let Some(ph) = photon {
+            data.push(ph.create_electron_array());
+        }
+        photon = None;
     }
 
+    let final_vec = data.iter().flatten().collect::<Vec<_>>();
 
-    //let _pack = inv.create_electron_event();
+
+
     
 }
