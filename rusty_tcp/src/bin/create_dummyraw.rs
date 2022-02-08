94f749ee9ee5341f6d81f491bbff3cc6b63960a3
yvesauad <yvesauad@gmail.com>
2022-02-08 14:22:53 +0100

    YA: generating random distributions

--- rusty_tcp/src/bin/create_dummyraw.rs
+++ rusty_tcp/src/bin/create_dummyraw.rs
@@ -1,11 +1,27 @@
 use timepix3::packetlib::*;
+use rand_distr::{Normal, Distribution};
+use rand::distributions::Uniform;
+use rand::{Rng, thread_rng};
 
 fn main() {
-    let inv = InversePacket::new_inverse_electron(10, 10, 10_000_000_000);
-    let tdc_inv = InversePacket::new_inverse_tdc(10_000_000_000);
+    //let inv = InversePacket::new_inverse_electron(10, 10, 10_000_000_000);
+    //let tdc_inv = InversePacket::new_inverse_tdc(10_000_000_000);
+
+
+    let npoints = 1_000;
+    let normal = Normal::new(100.0, 3.0).unwrap();
+    let uni = Uniform::new(0.0, 1.0);
+
+    for _ in 0..npoints {
+        let v = normal.sample(&mut thread_rng());
+        let pp = uni.sample(&mut thread_rng());
+        //let pp2 = thread_rng().sample(uni);
+        println!("{} and {}", v, pp);
+    }
 
     //inv.test_func();
     //inv.tdc_test_func();
+   
     
     
 
