2979866f21716f685b377c6f2d58268ed3b61924
yvesauad <yvesauad@gmail.com>
2022-02-15 08:43:33 +0100

    YA: removing file limit

--- rusty_tcp/src/postlib.rs
+++ rusty_tcp/src/postlib.rs
@@ -295,7 +295,6 @@ pub mod coincidence {
         while let Ok(size) = file.read(&mut buffer) {
             if size == 0 {println!("Finished Reading."); break;}
             total_size += size;
-            if total_size >= 5_000_000_000 {break;}
             println!("MB Read: {}", total_size / 1_000_000 );
             let mut temp_edata = TempElectronData::new();
             let mut temp_tdc = TempTdcData::new();
