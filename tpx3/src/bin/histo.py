e47c9d645795caba18a195864322f8f99ddedd65
yvesauad <yvesauad@gmail.com>
2022-10-07 16:01:23 +0200

    YA: improved histogram

--- tpx3/src/bin/histo.py
+++ tpx3/src/bin/histo.py
@@ -11,6 +11,7 @@ disparray = numpy.linspace(off, disp*SPIM_PIXELS, SPIM_PIXELS)
 t = numpy.fromfile("tH.txt", dtype='int64')
 tabs = numpy.fromfile("tabsH.txt", dtype='uint64')
 g2t = numpy.fromfile("g2tH.txt", dtype='int64')
+g2_total = numpy.fromfile("isi_g2.txt", dtype='int64')
 xT = numpy.loadtxt("spec.txt", delimiter=',')
 x = numpy.loadtxt("cspec.txt", delimiter=',')
 xH = numpy.fromfile("xH.txt", dtype='uint32')
@@ -24,13 +25,9 @@ indexes_position = numpy.where(xH < 256)
 indexes_time = numpy.where(tabs > 3*(max(tabs)+min(tabs))/4)
 indexes_extra = numpy.where(xH > 70)
 
-#yH = numpy.loadtxt("yH.txt", delimiter=',')
-#indexes = numpy.where(xH<1025)[0]
-#xH = xH[indexes]
-#t = t[indexes]
 
 #Getting CLE
-indexes_cle = numpy.where((numpy.abs(t + 500) < 200))
+indexes_cle = numpy.where((numpy.abs(t + 630) < 25))
 cle = numpy.zeros(1041)
 for val in xH[indexes_cle]:
     cle[val] += 1
@@ -38,9 +35,9 @@ for val in xH[indexes_cle]:
 
 #Getting g2
 indexes_g2 = numpy.where( (numpy.abs(g2t) < 25))
-#indexes_g2 = numpy.where( (numpy.abs(g2t) < 25) & (numpy.abs(t+500) < 100))
+indexes_g2_correlated = numpy.where( (numpy.abs(g2t) < 25) & (numpy.abs(t+630) < 25))
 g2 = numpy.zeros(1041)
-for val in xH[indexes_g2]:
+for val in xH[indexes_g2_correlated]:
     g2[val] += 1
 unique, counts = numpy.unique(g2t, return_counts = True)
 total = numpy.sum(counts)
@@ -48,7 +45,7 @@ g2_total_photons = numpy.sum(counts[1:])
 g2_photons = numpy.sum(g2)
 print(f'Total number of correlations is {total}. From these, {g2_total_photons} fall into the g2 statistics, and only {g2_photons} fall into the g2 curve, or {g2_photons * 100 /total}%.')
 
-#cRatio = numpy.divide(x, xT)
+#Performing the ratios
 cRatio = numpy.divide(cle, xT)
 cRatio2 = numpy.divide(g2, xT)
 
@@ -59,44 +56,55 @@ tbin = int((tmax - tmin)) + 1
 tbin = int(tbin/6)
 print(tmax, tmin, tbin)
 
-fig, ax = plt.subplots()
-ax2 = ax.twinx()
-ax.plot(disparray, cle, label='Correlated Data')
-ax.plot(disparray, xT, alpha=0.8, ls='-', color='red', lw=1, label='Total Data')
-ax2.plot(disparray, cRatio, alpha=0.8, ls='-', color='green', lw=1, label='Ratio CLE')
-ax2.plot(disparray, cRatio2, alpha=0.8, ls='--', color='red', lw=1, label='Ratio g2')
-ax.set_xlabel('Energy (eV)')
-ax.set_ylabel('$\Gamma^{Loss}$ (A.U.)')
-ax2.set_ylabel('Coincidence Ratio', c='green')
-ax.legend(loc='upper center')
-ax2.legend()
-#ax2.set_yscale("log")
+#Plot of the ratios
+fig, ax = plt.subplots(nrows=2)
+ax[0].plot(disparray, cle/max(cle), label='Correlated Data')
+ax[0].plot(disparray, g2/max(g2), label='Correlated Data (g2)')
+ax[0].plot(disparray, xT/max(xT), alpha=0.8, ls='-', color='red', lw=1, label='Total Data')
+ax[1].plot(disparray, cRatio/max(cRatio), alpha=0.8, ls='-', color='green', lw=1, label='Ratio CLE')
+ax[1].plot(disparray, cRatio2/max(cRatio2), alpha=0.8, ls='--', color='red', lw=1, label='Ratio g2')
+ax[0].set_xlabel('Energy (eV)')
+ax[0].set_ylabel('$\Gamma^{Loss}$ (A.U.)')
+ax[1].set_ylabel('Coincidence Ratio', c='green')
+ax[0].legend(loc='upper center')
+ax[1].legend()
+#ax[0].set_yscale("log")
+#ax[1].set_yscale("log")
 plt.tight_layout()
 
-fig, ax = plt.subplots(nrows=3, sharex=True)
-ax[0].hist(t[indexes2], bins=tbin, range=(tmin, tmax))
-ax[1].hist(t[indexes12], bins=tbin, range=(tmin, tmax))
-ax[2].hist(t[indexes_time], bins=tbin, range=(tmin, tmax))
+#Plot of the histograms
+fig, ax = plt.subplots(nrows=2, sharex=True)
+ax[0].hist(t[indexes2], bins=tbin, range=(tmin, tmax), alpha=0.2, color='red', label='Channel 0')
+ax[0].hist(t[indexes12], bins=tbin, range=(tmin, tmax), alpha=0.2, color='blue', label='Channel 12')
+ax[0].legend()
+ax[1].hist(t[indexes_g2], bins=tbin, range=(tmin, tmax), alpha=0.2, color='green', label='g2')
+ax[1].hist(t[indexes_g2_correlated], bins=tbin, range=(tmin, tmax), alpha=0.2, color='magenta', label='g2_eff')
+ax[1].legend()
 ax[1].set_xlabel('Time delay (units of 260 ps)')
 
-fig, ax = plt.subplots()
-ax.hist(tot, bins=25)
-ax.set_xlabel('Time over threshold (units of 1.5615 ns)')
+#Plot of the ToTs
+#fig, ax = plt.subplots()
+#ax.hist(tot, bins=25)
+#ax.set_xlabel('Time over threshold (units of 1.5615 ns)')
 
-fig, ax = plt.subplots()
-ax.hist2d(xH, g2t, bins=[int(SPIM_PIXELS/2), 1001], range=[[0, SPIM_PIXELS], [-500, 500]])
-#ax.hist(g2t[indexes_position], bins=1000)
-ax.set_xlabel('Energy (pixels)')
-ax.set_ylabel('Photon correlation delay (units of IsiBox 120 ps)')
+#Histogram of the g2
+#fig, ax = plt.subplots()
+#ax.hist2d(xH, g2t, bins=[int(SPIM_PIXELS/2), 1001], range=[[0, SPIM_PIXELS], [-500, 500]])
+#ax.set_xlabel('Energy (pixels)')
+#ax.set_ylabel('Photon correlation delay (units of IsiBox 120 ps)')
 
+#Histogram of the correlated/non-correlated g2
 fig, ax = plt.subplots()
-ax.hist(g2t, bins=1001, range=(-500, 500))
+ax.hist(g2t[indexes_g2], bins=2001, range=(-1000, 1000), label='Correlated', alpha=0.2)
+ax.hist(g2t, bins=2001, range=(-1000, 1000), label='Total', alpha=0.2)
+ax.legend()
 ax.set_xlabel('Photon correlation delay (units of IsiBox 120 ps)')
 
-fig, ax = plt.subplots(ncols=3, sharey=True)
-ax[0].hist2d(xH[indexes2], t[indexes2], bins=[SPIM_PIXELS, tbin], range = [[0, SPIM_PIXELS], [tmin, tmax]], cmap = 'viridis', norm = mcolors.PowerNorm(0.2))
-ax[1].hist2d(xH[indexes12], t[indexes12], bins=[SPIM_PIXELS, tbin], range = [[0, SPIM_PIXELS], [tmin, tmax]], cmap = 'viridis', norm = mcolors.PowerNorm(0.2))
-ax[2].hist2d(xH[indexes_g2], t[indexes_g2], bins=[SPIM_PIXELS, tbin], range = [[0, SPIM_PIXELS], [tmin, tmax]], cmap = 'viridis', norm = mcolors.PowerNorm(0.2))
+#Histogram of the CLE
+fig, ax = plt.subplots(ncols=2, sharey=True)
+ax[0].hist2d(xH, t, bins=[SPIM_PIXELS, tbin], range = [[0, SPIM_PIXELS], [tmin, tmax]], cmap = 'viridis', norm = mcolors.PowerNorm(0.2))
+#ax[1].hist2d(xH[indexes12], t[indexes12], bins=[SPIM_PIXELS, tbin], range = [[0, SPIM_PIXELS], [tmin, tmax]], cmap = 'viridis', norm = mcolors.PowerNorm(0.2))
+ax[1].hist2d(xH[indexes_g2], t[indexes_g2], bins=[SPIM_PIXELS, tbin], range = [[0, SPIM_PIXELS], [tmin, tmax]], cmap = 'viridis', norm = mcolors.PowerNorm(0.2))
 ax[0].set_xlabel('Energy (pixels)')
 ax[1].set_xlabel('Energy (pixels)')
 ax[0].set_ylabel('Time delay (units of 260 ps)')
