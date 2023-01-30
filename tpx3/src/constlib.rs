d616f46bfe5d07efd9d22070cf1f126c4eecb04d
yvesauad <yvesauad@gmail.com>
2023-01-30 23:01:21 +0100

    YA: 128MB standard in the isibuffer

--- tpx3/src/constlib.rs
+++ tpx3/src/constlib.rs
@@ -1,5 +1,5 @@
 //Coincidence values using the IsiBox//
-pub const ISI_BUFFER_SIZE: usize = 512_000_000; //Buffer size when reading files
+pub const ISI_BUFFER_SIZE: usize = 128_000_000; //Buffer size when reading files
 pub const ISI_TIME_WIDTH: u64 = 200; //Time width considered for coincidence (units of 1.5625 ns)
 pub const ISI_TIME_DELAY: u64 = 78; //Time delay considered for coincidence (units of 1.5625 ns)
 pub const ISI_TP3_MAX_DIF: u64 = 1000; //Maximum clock difference to synchronize IsiBox and Timepix3 (in units of 1.5625 ns)
