456175fa088a02c2c28e31f31ef18bff251762e3
yvesauad <yvesauad@gmail.com>
2023-09-08 09:56:43 +0200

    YA: fixing the instructions

--- tpx3/src/bin/time_resolved.rs
+++ tpx3/src/bin/time_resolved.rs
@@ -11,12 +11,7 @@ fn main() -> Result<(), Tp3ErrorKind> {
     println!("
     ***Instructions***:
 
-    Turn the script using the --release tag. At least a single argument must be parsed, which is the folder containing your .tpx3 data. Data must be accompanied with a json file, with matching names.
-    Example:
-
-    >>  cargo run --bin time-resolved --release 'C:\\Users\\SpongeBob\\Data'
-    
-    >>  cargo run --bin time-resolved --release 'C:\\Users\\SpongeBob\\Data' 1
+    At least a single argument must be parsed, which is the folder containing your (multiple) .tpx3 data. Data must be accompanied with a json file, with matching names. If there are more than one .tpx3 file, data treatment is done in parallel.
 
     Example of the json file and the required fields:
 
@@ -26,13 +21,12 @@ fn main() -> Result<(), Tp3ErrorKind> {
         -> (mode == 2) => Hyperspectral image;
         -> (mode != 2) => 4D image;
             o xscan_size & yscan_size = Spatial sampling;
-        -> Cluster correction can be activated by parsing a second argument, varying from 0 to 5. There are three automatic cluster correction:
+        -> Cluster correction can be activated by parsing a second argument, varying from 0 to 5. There are three automatic cluster correction settings:
             o '0' => No correction;
             o '1' => Average;
             o '2' => Maximum ToT;
         -> The other fields of the json are currently not considered, but they give you the parameters you have used during data
         acquisition. sup0 & sup1, for example, are the EELS dispersion and offset, respectively;
-        -> The time delay and time width are defined at compile-time, so you should change them at constlib.rs instead;
 
     "
     );
--- tpx3/src/bin/tp3_coin.rs
+++ tpx3/src/bin/tp3_coin.rs
@@ -11,23 +11,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("
     ***Instructions***:
 
-    Turn the script using the --release tag. A single argument must be parsed, which is the folder containing your .tpx3 data. Data must be accompanied with a json file, with matching names. Parsing other arguments will be ignored🙅‍♂️.
-    Example:
-
-    >>  cargo run --bin tp3_coin --release 'C:\\Users\\SpongeBob\\Data'
+    At least a single argument must be parsed, which is the folder containing your (multiple) .tpx3 data. Data must be accompanied with a json file, with matching names. If there are more than one .tpx3 file, data treatment is done in parallel.
 
     Example of the json file and the required fields:
 
     {{bin: false, bytedepth: 2, cumul: false, mode: 0, xspim_size: 10, yspim_size: 10, xscan_size: 512, yscan_size: 512, pixel_time: 320, time_delay: 0, time_width: 0, spimoverscanx: 1, spimoverscany: 1, save_locally: true, sup0: 0.0155, sup1: 0.0}}
 
+
     For this particular script:
-        🤜 (mode== 0) => No hyperspectral image;
-        🤜 (mode!= 0) => Hyperspectral image;
-            👀 xscan_size & yscan_size => Hyperspectral image sampling;
-        🔧 Cluster correction is deactivated. Please request if you wish to do o;
-        🕯️ The other fields of the json are currently not considered, but they give you the parameters you have used during data
+        -> (mode== 0) => No hyperspectral image;
+        -> (mode!= 0) => Hyperspectral image;
+            o xscan_size & yscan_size => Hyperspectral image sampling;
+        -> Cluster correction can be activated by parsing a second argument, varying from 0 to 5. There are three automatic cluster correction settings:
+            o '0' => No correction;
+            o '1' => Average;
+            o '2' => Maximum ToT;
+        -> The other fields of the json are currently not considered, but they give you the parameters you have used during data
         acquisition. sup0 & sup1, for example, are the EELS dispersion and offset, respectively;
-        💡 The time delay and time width are defined at compile-time, so you should change them at constlib.rs insted;
+        -> The time delay and time width are defined at compile-time, so you should change them at constlib.rs insted;
 
     "
     );
