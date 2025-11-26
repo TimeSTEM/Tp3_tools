897b0cffb8e7b55ca817837279463352a196a6e3
yvesauad <yvesauad@gmail.com>
2025-11-26 11:56:10 +0100

    YA: testing on linux

---
+++ tpx3/build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // Where to find TTX.so at link time
+    println!("cargo:rustc-link-search=native=lib");
+
+    // Linux runtime loader path
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/lib");
+
+    // Tell Rust which library to link
+    #[cfg(target_os = "windows")]
+    println!("cargo:rustc-link-lib=dylib=TTX");      // TTX.dll
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=dylib=TTX");      // Rust will look in lib/ for libTTX.so
+}
+
